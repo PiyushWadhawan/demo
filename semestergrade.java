c94b7dc8567a48762d4f98584f9ffd0a3fc6968f
PiyushWadhawan <66672056+PiyushWadhawan@users.noreply.github.com>
2022-04-29 10:00:56 +0530

    Update semestergrade.java

--- semestergrade.java
+++ semestergrade.java
@@ -2,6 +2,13 @@ public class semestergrade
 {
 	public static void main(String[] args) 
 	{
-        System.out.println("Hello, World!"); 
+	System.out.println("Course               Credits");
+        System.out.println("DevOps                 : 3");
+        System.out.println("Communication          : 1");
+        System.out.println("Environment            : 1");
+        System.out.println("HPC                    : 4");
+        System.out.println("Undergraduate Research : 2");
+        System.out.println("Career skills          : 3");
+        System.out.println("Seminar on special topics : 2");
 	}
 }
